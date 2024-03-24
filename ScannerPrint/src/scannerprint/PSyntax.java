7c20fb19a17ff9738c83335fb13b284eae25606e
Konstantin <zdrastekostik@gmail.com>
2024-03-24 14:40:23 +0500

    Implemented all functions from Assign() to Factor() in PSyntax class.
    Not inmplemented function MatchMust() look for it in the main project in Lego folder.

--- ScannerPrint/src/scannerprint/PSyntax.java
+++ ScannerPrint/src/scannerprint/PSyntax.java
@@ -34,47 +34,83 @@ public class PSyntax {
 	
 	boolean OrOr()
 	{
-		
+		if (!AndAnd()) return false;
+	    while (parent_scanner.Match(Lexemme.TPriority.OrOr)) {
+		   if (!AndAnd()) return false;
+		}
+	    return true;
 	}
 	
 	boolean Or()
 	{
-		
+		if (!XOr()) return false;
+	    while (parent_scanner.Match(Lexemme.TPriority.Or)) {
+			if (!XOr()) return false;
+		}
+	    return true;
 	}
 	
 	boolean AndAnd()
 	{
-		
+		if (!Or()) return false;
+	    while (parent_scanner.Match(Lexemme.TPriority.AndAnd)) {
+		   if (!Or()) return false;
+		}
+	    return true;
 	}
 	
 	boolean And()
 	{
-		
+	    if (!Equ()) return false;
+	    while (parent_scanner.Match(Lexemme.TPriority.And)) {
+			if (!Equ()) return false;
+		}
+	    return true;
 	}
 	
 	boolean xOr()
 	{
-		
+		if (!And()) return false;
+	    while (parent_scanner.Match(Lexemme.TPriority.XOr)) {
+			if (!And()) return false;
+		}
+	    return true;
 	}
 	
 	boolean Equ()
 	{
-		
+		if (!Compare()) return false;
+		while (parent_scanner.Match(Lexemme.TPriority.Equ)) {
+			if (!Compare()) return false;
+		}
+		return true;
 	}
 	
 	boolean Compare()
 	{
-		
+		if (!Shift()) return false;
+	    while (parent_scanner.Match(Lexemme.TPriority.Compare)) {
+			if (!Shift()) return false;
+		}
+	    return true;
 	}
 	
 	boolean Shift()
 	{
-		
+		if (!Term()) return false;
+	    while (parent_scanner.Match(Lexemme.TPriority.Shift)) {
+			if (!Term()) return false;
+		}
+	    return true;
 	}
 	
 	boolean Factor()
 	{
-		
+		if (!Factor()) return false;
+		while (parent_scanner.Match(Lexemme.TPriority.Term)) {
+			if (!Factor()) return false;
+		}
+	    return true;
 	}
 	
 	boolean Unary()
